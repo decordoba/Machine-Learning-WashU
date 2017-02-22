a769fe5062db7c121b11060230d82dbe5d670b20
decordoba <danieldecordobagil@hotmail.com>
2017-02-21 20:32:48 -0600

    Fixed genTrainFeatures to work in windows; training sets (csv) added

--- project2/genTrainFeatures.m
+++ project2/genTrainFeatures.m
@@ -9,17 +9,24 @@ function [x,y]=genTrainFeatures()
 % x: n feature vectors of dimensionality d [d,n]
 % y: n labels (+1 = girl, -1 = boy)
 
-% convert girl names into feature vectors
-system('cat girls.train | python name2features.py > girls.csv ')
-system('cat boys.train | python name2features.py > boys.csv ')
-
+%% convert girl and boy names into feature vectors
+if ispc() % Stdin works differently for Windows and Unix
+    system('python name2features.py < girls.train > girls.csv');
+    system('python name2features.py < boys.train > boys.csv');
+else
+    system('cat girls.train | python name2features.py > girls.csv ');
+    system('cat boys.train | python name2features.py > boys.csv ');
+end
+    
 %% load in girls
 girls=load('girls.csv');
 [n1,d1]=size(girls);
+
 %% load in boys
 boys=load('boys.csv');
 [n2,d2]=size(boys);
 
+%% create x and y from girls and boys
 assert(d1==d2);
 x=[girls;boys]';
 y=[ones(n1,1);-ones(n2,1)]';
@@ -29,5 +36,4 @@ ii=randperm(length(y));
 x=x(:,ii);
 y=y(ii);
 
-
-
+end
