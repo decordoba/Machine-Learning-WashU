815b67af1e83b5755999f88b7a9c7bcae6cc5ffd
decordoba <danieldecordobagil@hotmail.com>
2017-02-24 13:43:58 -0600

    Fixed whoareyou.m format

--- project2/whoareyou.m
+++ project2/whoareyou.m
@@ -1,23 +1,28 @@
-function whoareyou(w,b);
+function whoareyou(w, b)
 % function whoareyou(w,b);
 % 
 % A little interactive demo of your name classifier
 
-if nargin<2, b=0;end;
+if nargin<2
+    b = 0;
+end
 
-	while true,
-		name=input('Who are you>','s');
-		if strcmp(name, 'byebye') == 1,
-			break
-		end;
-		system(['echo ' name ' | python name2features.py > tempfeatures.csv' ]);
-		x=load('tempfeatures.csv')';
+while true
+    name = input('Who are you>', 's');
+    if strcmp(name, 'byebye') == 1,
+        break
+    end;
+    
+    system(['echo ' name ' | python name2features.py > tmpfeatures.csv' ]);
+    x = load('tmpfeatures.csv')';
 
-		pred=classifyLinear(x,w,b);
-		if pred>0,
-			out=[name ', I am sure you are a nice girl.'];
-		else,
-			out=[name ', I am sure you are a nice boy.'];
-		end;
-		disp(out);
-	end;
+    pred = classifyLinear(x, w, b);
+    if pred > 0,
+        out = [name ', I am sure you are a nice girl.'];
+    else
+        out = [name ', I am sure you are a nice boy.'];
+    end;
+    disp(out);
+end;
+
+end
